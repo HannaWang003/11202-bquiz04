f208676af610fcf565b2c1db76d9dcab7e7ffb03
Hanna <hannawang003@gmail.com>
2024-01-22 16:05:08 +0800

    把function login放到外部檔案，location改用switch判斷

--- front/admin.php
+++ front/admin.php
@@ -23,27 +23,3 @@
     </tr>
 </table>
 <div class="ct"><button onclick="login('admin')">確認</button></div>
-<script>
-function login(table) {
-    $.get("./api/chk_ans.php", {
-            ans: $("#ans").val()
-        },
-        (chk) => {
-            if (parseInt(chk) == 0) {
-                alert("對不起，您輸入的驗證碼有誤，請您重新登入")
-            } else {
-                $.post("./api/chk_pw.php", {
-                    table,
-                    acc: $("#acc").val(),
-                    pw: $("#pw").val()
-                }, (res) => {
-                    if (parseInt(res) == 0) {
-                        alert("帳號或密碼錯誤,請重新輸入");
-                    } else {
-                        location.href = "back.php";
-                    }
-                })
-            }
-        })
-}
-</script>
