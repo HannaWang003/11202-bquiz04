b06f3727c6565d4fe02a539b0ca09f52decfaae3
Hanna <hannawang003@gmail.com>
2024-01-31 08:37:49 +0800

    完成會員及管理者的登出及index-menu的切換

--- back.php
+++ back.php
@@ -1,8 +1,7 @@
 <?php
 include_once "./api/db.php";
 ?>
-<!DOCTYPE html
-    PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!-- saved from url=(0057)?do=admin -->
 <html xmlns="http://www.w3.org/1999/xhtml">
 
@@ -31,19 +30,19 @@ include_once "./api/db.php";
                 <a href="?do=mem">會員管理</a>
                 <a href="?do=bot">頁尾版權管理</a>
                 <a href="?do=news">最新消息管理</a>
-                <a href="?do=logout" style="color:#f00;">登出</a>
+                <a href="./api/logout.php" style="color:#f00;">登出</a>
             </div>
         </div>
         <div id="right">
             <?php
-			$do = $_GET['do'] ?? 'admin';
-			$file = "./back/{$do}.php";
-			if (file_exists($file)) {
-				include $file;
-			} else {
-				include "./back/admin.php";
-			}
-			?>
+            $do = $_GET['do'] ?? 'admin';
+            $file = "./back/{$do}.php";
+            if (file_exists($file)) {
+                include $file;
+            } else {
+                include "./back/admin.php";
+            }
+            ?>
         </div>
         <div id="bottom" style="line-height:70px; color:#FFF; background:url(icon/bot.png);" class="ct">
             <?= $Bottom->find(1)['bottom'] ?></div>
