e0ce14520a8762030015cd0529bc39a62db7e6a0
Hanna <hannawang003@gmail.com>
2024-01-31 16:25:52 +0800

    result: tmp->tmp_name

--- api/save_goods.php
+++ api/save_goods.php
@@ -1,8 +1,8 @@
 <?php
 include_once "db.php";
-if (!empty($_FILES['img']['tmp'])) {
+if (!empty($_FILES['img']['tmp_name'])) {
     $_POST['img'] = $_FILES['img']['name'];
-    move_uploaded_file($_FILES['img']['tmp'], "../img/{$_FILES['img']['name']}");
+    move_uploaded_file($_FILES['img']['tmp_name'], "../img/{$_FILES['img']['name']}");
 }
 $_POST['no'] = rand(100000, 999999);
 $_POST['sh'] = 1;
