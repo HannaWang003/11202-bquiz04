7fea0347dc373217c5b4a79f20c34166b539d83b
HannaWang003 <hannawang003@gmail.com>
2024-02-04 17:14:15 +0800

    完成商品分類顯示畫面

--- back/th.php
+++ back/th.php
@@ -1,1 +1,26 @@
-<h1>th</h1>
+<h1 class="ct">商品分類</h1>
+<div class="ct">
+    <span>新增大分類</span>
+    <input type="text" name="big" id="big">
+    <button>新增</button>
+</div>
+<div class="ct">
+    <span>新增中分類</span>
+    <select name="big" id="bigs"></select>
+    <input type="text" name="mid" id="mid">
+    <button>新增</button>
+</div>
+<table class="all">
+    <tr>
+        <td class="tt">流行皮件</td>
+        <td class="ct tt">
+            <button>修改</button><button>刪除</button>
+        </td>
+    </tr>
+    <tr>
+        <td class="ct pp">女用皮件</td>
+        <td class="ct pp">
+        <button>修改</button><button>刪除</button>
+        </td>
+    </tr>
+</table>
