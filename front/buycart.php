c0a7d66a410229b55857db701fb7adaaa7bc84a8
Hanna <hannawang003@gmail.com>
2024-02-16 14:29:05 +0800

    測試購物車功能可以串接

--- front/buycart.php
+++ front/buycart.php
@@ -1,2 +1,7 @@
 <?php
 if (!isset($_SESSION['mem'])) to("?do=login");
+if (!isset($_GET['id'])) {
+    echo "<h2 class='ct'>購物車中尚無商品</h2>";
+} else {
+    echo "<h2 class='ct'>" . $_SESSION['mem'] . "的購物車</h2>";
+}
--- front/main.php
+++ front/main.php
@@ -21,58 +21,56 @@ if ($type == 0) {
 ?>
 <h2><?= $nav; ?></h2>
 <style>
-.item {
-    width: 80%;
-    height: 160px;
-    margin: 5px auto;
-    display: flex;
-}
+    .item {
+        width: 80%;
+        height: 160px;
+        margin: 5px auto;
+        display: flex;
+    }
 
-.item .img {
-    width: 33%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    border: 2px solid #fff;
-}
-
-.item .info {
-    width: 65%;
-    display: flex;
-    flex-direction: column;
-
-    div {
+    .item .img {
+        width: 33%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
         border: 2px solid #fff;
-        border-left: 0;
-        border-top: 0;
-        flex-grow: 1;
     }
 
-    div:nth-child(1) {
-        border-top: 2px solid #fff;
+    .item .info {
+        width: 65%;
+        display: flex;
+        flex-direction: column;
+
+        div {
+            border: 2px solid #fff;
+            border-left: 0;
+            border-top: 0;
+            flex-grow: 1;
+        }
+
+        div:nth-child(1) {
+            border-top: 2px solid #fff;
+        }
     }
-}
 </style>
 <?php
 foreach ($goods as $good) {
 ?>
-<div class="item">
-    <div class="img pp">
-        <a href="?do=detail&id=<?=$good['id'];?>">
-            <img src="./img/<?=$good['img'];?>" style="width:80%;height:110px">
-        </a>
-    </div>
-    <div class="info pp">
-        <div class='ct tt'><?= $good['name'] ?></div>
-        <div>價錢:<?= $good['price'] ?>
-            <a href="?do=detail&id=<?=$good['id'];?>">
-                <img src="./icon/0402.jpg" style='float:right'>
+    <div class="item">
+        <div class="img pp">
+            <a href="?do=detail&id=<?= $good['id']; ?>">
+                <img src="./img/<?= $good['img']; ?>" style="width:80%;height:110px">
             </a>
         </div>
-        <div>規格:<?= $good['spec'] ?></div>
-        <div>簡介:<?= mb_substr($good['intro'], 0, 25) ?>...</div>
+        <div class="info pp">
+            <div class='ct tt'><?= $good['name'] ?></div>
+            <div>價錢:<?= $good['price'] ?>
+                <img src="./icon/0402.jpg" style='float:right' onclick="location.href='?do=buycart&id=<?= $good['id'] ?>&qt=1'">>
+            </div>
+            <div>規格:<?= $good['spec'] ?></div>
+            <div>簡介:<?= mb_substr($good['intro'], 0, 25) ?>...</div>
+        </div>
     </div>
-</div>
 
 <?php
 }
