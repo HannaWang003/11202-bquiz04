83747d7aecf4a4913de9af37bbb166ed7d6e86a5
Hanna <hannawang003@gmail.com>
2024-02-16 11:36:57 +0800

    debug

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
 include_once "./api/db.php";
 ?>
-<!DOCTYPE html
-    PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!-- saved from url=(0039) -->
 <html xmlns="http://www.w3.org/1999/xhtml">
 
@@ -15,18 +14,18 @@ include_once "./api/db.php";
     <script src="./js/jquery-1.9.1.min.js"></script>
 </head>
 <style>
-#top {
-    display: flex;
-    justify-content: space-between;
+    #top {
+        display: flex;
+        justify-content: space-between;
 
-    a {
-        width: 50%;
-    }
+        a {
+            width: 50%;
+        }
 
-    img {
-        width: 100%;
+        img {
+            width: 100%;
+        }
     }
-}
 </style>
 
 <body>
@@ -44,11 +43,11 @@ include_once "./api/db.php";
                 <?php
                 if (isset($_SESSION['mem'])) {
                 ?>
-                <a onclick="logout('mem')">登出</a>
+                    <a onclick="logout('mem')">登出</a>
                 <?php
                 } else {
                 ?>
-                <a href="?do=login">會員登入</a>
+                    <a href="?do=login">會員登入</a>
                 <?php
                 }
                 ?>
@@ -56,11 +55,11 @@ include_once "./api/db.php";
                 <?php
                 if (isset($_SESSION['admin'])) {
                 ?>
-                <a href="back.php">回管理頁</a>
+                    <a href="back.php">回管理頁</a>
                 <?php
                 } else {
                 ?>
-                <a href="?do=admin">管理登入</a>
+                    <a href="?do=admin">管理登入</a>
                 <?php
                 }
                 ?>
@@ -70,35 +69,15 @@ include_once "./api/db.php";
         <div id="left" class="ct">
             <div style="min-height:400px;">
                 <a href="">全部商品</a>
-                <?php
-$bigs=$Type->all(['big_id'=>0]);
-foreach($bigs as $big){
-                ?>
-                <div class="ww">
-                    <a href=""><?=$big['name']?></a>
-                    <div class="s">
-                        <?php
-                if($Type->count(['big_id'=>$big['id']])>0){
-                    $mids=$Type->all(['big_id'=>$big['id']]);
-                    foreach($mids as $mid){
-?>
-                        <a href=""><?=$mid['id']?></a>
-                        <?php
-                        }
-
-                }
-                }
-                ?>
-                    </div>
-                </div>
-                <span>
-                    <div>進站總人數</div>
-                    <div style="color:#f00; font-size:28px;">
-                        00005 </div>
-                </span>
             </div>
-            <div id="right">
-                <?php
+            <span>
+                <div>進站總人數</div>
+                <div style="color:#f00; font-size:28px;">
+                    00005 </div>
+            </span>
+        </div>
+        <div id="right">
+            <?php
             $do = $_GET['do'] ?? "main";
             $file = "./front/{$do}.php";
             if (file_exists($file)) {
@@ -108,7 +87,7 @@ foreach($bigs as $big){
             }
 
             ?>
-            </div>
+
         </div>
         <div id="bottom" style="line-height:70px;background:url(./icon/bot.png); color:#FFF;" class="ct">
             <?= $Bot->find(1)['bot'] ?> </div>
