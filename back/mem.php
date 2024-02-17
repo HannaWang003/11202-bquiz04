a904947db57aa2811a27557bc50a3b8f03e1f075
HannaWang003 <hannawang003@gmail.com>
2024-02-17 12:45:12 +0800

    處理後台會員管理系統

--- back/mem.php
+++ back/mem.php
@@ -1,1 +1,15 @@
-<h1>mem</h1>
+<h1>會員管理</h1>
+<table class="all">
+    <tr>
+        <th class="ct tt">姓名</th>
+        <th class="ct tt">會員帳號</th>
+        <th class="ct tt">註冊日期</th>
+        <th class="ct tt">操作</th>
+    </tr>
+    <tr>
+        <td class="ct pp"></td>
+        <td class="ct pp"></td>
+        <td class="ct pp"></td>
+        <td class="ct pp"></td>
+    </tr>
+</table>
